0520de7eef968d4b6383804d01057bf38026a235
Inserer_le_nom <77113428+joachim-fr@users.noreply.github.com>
2025-04-05 15:53:07 +0200

    a

--- js/script.js
+++ js/script.js
@@ -1,74 +1,107 @@
 let temp = 20;
 let DernierClic = 0;
 let now = 0;
-let Facteurderefroidissement = 0.99;
 let succes = [false, false, false, false, false, false, false, false, false, false];
 let succestotal = succes.length;
 let nowsucces = 0;
 let succesunlocked = 0;
 
 function updateTemperature() {
-    $("#temp").text(temp);
+    $("#temp").text(Math.round(temp));
 }
 
 $(".bite").click(function() {
-    console.log("Bite clicked!");
-    temp ++;
-    DernierClic = Date.now();
+    const maintenant = Date.now();
+    const deltanow = maintenant - DernierClic;
+    
+    if (!succes[0]) {
+        succes[0] = true
+        notifsucces(1);
+    }
+
+    if (deltanow < 300) {
+        trerapidefavetier++;
+    }
+    else {
+        trerapidefavetier = 0;
+    }
+
+    DernierClic = maintenant;
+    temp += 1 + (trerapidefavetier * 0.5);
     updateTemperature();
 });
 
-function vartemp(){
+function vartemp() {
     now = Date.now();
-    if (now - DernierClic > 10000) {
-        if (temp + 32 > 0) {
-            temp -= 32;
-            temp -= temp / Math.exp(Facteurderefroidissement);
+    if (now - DernierClic > 3000) {
+        if (temp >= 32) {
+            temp -= temp * 0.05;
+        } else if (temp < 32) {
+            temp += (32 - temp) * 0.1;
         }
     }
-
-    temp = Math.round(temp) + 32;
     console.log("temp: " + temp);
     updateTemperature();
 }
+    
 
-function stautsucceupdate(succes) {
+function stautsucceupdate(succes, temperature) {
     $("#succes" + succes).removeClass("locked");
     succesunlocked += 1;
     $("#succesrempostes").text(succesunlocked);
+    $("#Tempsuccesdiv" + succes).show();
+    $("#Tempsucces" + succes).html(Math.round(temperature));
 }
 
-function updatesucces() {
-    if (temp >= 50 && !succes[0]) {
-        succes[0] = true;
-        notifsucces(1);
-        nowsucces = Date.now();
-        console.log(nowsucces);
-    }
+function updatesuccestypeT() {
+
     if (temp >= 100 && !succes[1]) {
         succes[1] = true;
         notifsucces(2);
+        stautsucceupdate(2, temp);
     }  
     if (temp >= 200 && !succes[2]) {
         succes[2] = true;
         notifsucces(3);
+        stautsucceupdate(3, temp);
     }
+    if (temp >= 1250 && !succes[3]){
+        succes[3] = true;
+        notifsucces(4);
+        stautsucceupdate(4, temp);
+    }
+
 }
 
 function updatebite() {
     const bite = document.getElementById("bite");
 
     if (temp > 100 && temp < 200) {
+        bite.style.animation = "tremblementRotation 1s linear infinite";
         bite.style.animationDuration = "0.5s";
         bite.style.color = "#ff8a8a";
     }
     else if (temp > 200 && temp < 300) {
+        bite.style.animation = "tremblementRotation 1s linear infinite";
         bite.style.animationDuration = "0.1s";
         bite.style.color = "#fa6161";
     }
-    else if (temp > 300) {
+    else if (temp > 300 && temp < 400) {
+        bite.style.animation = "tremblementRotation 1s linear infinite";
         bite.style.animationDuration = "0.01s";
         bite.style.color = "#ff0000";
+    }
+    else if (temp > 400 && temp < 500) {
+        bite.style.animation = "roatationbrulante 1s linear infinite";
+        bite.style.color = "#edb974";
+    }   
+    else if (temp > 500 && temp < 600) {
+        bite.style.animation = "roatationbrulante 0.5s linear infinite";
+        bite.style.color = "#edb974";
+    }
+    else if (temp > 600) {
+        bite.style.animation = "roatationbrulante 0.1s linear infinite";
+        bite.style.color = "#edb974";
     }
     else {
         bite.style.animationDuration = "5s";
@@ -81,7 +114,6 @@ function notifsucces(succes) {
     setTimeout(function() {
         $("#succesnotif" + succes).removeClass("show"); 
     }, 2000);
-    stautsucceupdate(succes);
 }
 
 $("#button1").click(function() {
@@ -109,10 +141,14 @@ $("#imgcroix").hover(
   }
 );
 
+for (let i = 1; i <= succestotal; i++) {
+    $("#Tempsuccesdiv" + i).hide();
+}
+
 $("#succesentout").text(succestotal)
 
 $("#succespage").hide();
 
 setInterval(vartemp, 1000);
 setInterval(updatebite, 10);
-setInterval(updatesucces, 10);
+setInterval(updatesuccestypeT, 10);
